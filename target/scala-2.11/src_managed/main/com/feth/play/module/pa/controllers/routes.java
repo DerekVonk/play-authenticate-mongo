ff34804ffce58e63760bdbc99115e3020adabd24
Derek Vonk <derek.vonk@globalorange.nl>
2016-04-18 10:45:17 +0200

    initial commit

---
+++ target/scala-2.11/src_managed/main/com/feth/play/module/pa/controllers/routes.java
@@ -0,0 +1,20 @@
+// @SOURCE:/home/derek/projects/play-authenticate-mongo/conf/routes
+// @HASH:6caaefd4ef4f1d428984c768556744679d050c00
+// @DATE:Mon Apr 18 10:25:23 CEST 2016
+
+package com.feth.play.module.pa.controllers;
+
+public class routes {
+public static final com.feth.play.module.pa.controllers.ReverseAuthenticate Authenticate = new com.feth.play.module.pa.controllers.ReverseAuthenticate();
+
+public static class javascript {
+public static final com.feth.play.module.pa.controllers.javascript.ReverseAuthenticate Authenticate = new com.feth.play.module.pa.controllers.javascript.ReverseAuthenticate();
+}
+          
+
+public static class ref {
+public static final com.feth.play.module.pa.controllers.ref.ReverseAuthenticate Authenticate = new com.feth.play.module.pa.controllers.ref.ReverseAuthenticate();
+}
+          
+}
+          
